ad2217c8457c19efb7410792ef41c21d437dc243
Sayanee Basu <sayanee@gmail.com>
2017-01-29 14:24:42 +0800

    fix for macOS Sierra and nodeMCU

--- examples/blinky/blinky.c
+++ examples/blinky/blinky.c
@@ -3,7 +3,7 @@
 #include "gpio.h"
 #include "os_type.h"
 
-static const int pin = 1;
+static const int pin = 2;
 static volatile os_timer_t some_timer;
 
 void some_timerfunc(void *arg)
@@ -12,12 +12,12 @@ void some_timerfunc(void *arg)
   if (GPIO_REG_READ(GPIO_OUT_ADDRESS) & (1 << pin))
   {
     // set gpio low
-    gpio_output_set(0, (1 << pin), 0, 0);
+    gpio_output_set(0, (1 << pin), (1 << pin), 0);
   }
   else
   {
     // set gpio high
-    gpio_output_set((1 << pin), 0, 0, 0);
+    gpio_output_set((1 << pin), 0, (1 << pin), 0);
   }
 }
 
@@ -27,7 +27,7 @@ void ICACHE_FLASH_ATTR user_init()
   gpio_init();
 
   // configure UART TXD to be GPIO1, set as output
-  PIN_FUNC_SELECT(PERIPHS_IO_MUX_U0TXD_U, FUNC_GPIO1); 
+  PIN_FUNC_SELECT(PERIPHS_IO_MUX_U0TXD_U, FUNC_GPIO1);
   gpio_output_set(0, 0, (1 << pin), 0);
 
   // setup timer (500ms, repeating)
